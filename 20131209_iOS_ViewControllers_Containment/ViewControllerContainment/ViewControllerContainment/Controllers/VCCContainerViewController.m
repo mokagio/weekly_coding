a51fe5f6ba2d228eeace547e60548603334bf96e
Gio <giovanni.lodi42@gmail.com>
2013-12-09 15:53:19 +0000

    Added comments

--- 20131209_iOS_ViewControllers_Containment/ViewControllerContainment/ViewControllerContainment/Controllers/VCCContainerViewController.m
+++ 20131209_iOS_ViewControllers_Containment/ViewControllerContainment/ViewControllerContainment/Controllers/VCCContainerViewController.m
@@ -21,6 +21,15 @@
 {
     [super viewDidLoad];
     
+//    See:
+//    https://developer.apple.com/library/ios/featuredarticles/ViewControllerPGforiPhoneOS/CreatingCustomContainerViewControllers/CreatingCustomContainerViewControllers.html
+//    
+//    Here’s what the code does:
+//    
+//    1. It calls the container’s addChildViewController: method to add the child. Calling the addChildViewController: method also calls the child’s willMoveToParentViewController: method automatically.
+//    2. It accesses the child’s view property to retrieve the view and adds it to its own view hierarchy. The container sets the child’s size and position before adding the view; containers always choose where the child’s content ap`pears. Although this example does this by explicitly setting the frame, you could also use layout constraints to determine the view’s position.
+//    3. It explicitly calls the child’s didMoveToParentViewController: method to signal that the operation is complete.
+    
     VCCButtonsViewController *buttonsViewController = [[VCCButtonsViewController alloc] init];
     [self addChildViewController:buttonsViewController];
     buttonsViewController.view.frame = CGRectMake(0, 0, self.view.frame.size.width, 140);
