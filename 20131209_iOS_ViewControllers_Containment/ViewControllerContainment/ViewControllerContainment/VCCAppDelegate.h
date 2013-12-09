a2ade390d050f51ff7b6b1f28065484c85894dd6
Gio <giovanni.lodi42@gmail.com>
2013-12-09 14:58:03 +0000

    iOS ViewControllers Containment: empty project

---
+++ 20131209_iOS_ViewControllers_Containment/ViewControllerContainment/ViewControllerContainment/VCCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VCCAppDelegate.h
+//  ViewControllerContainment
+//
+//  Created by Gio on 09/12/2013.
+//  Copyright (c) 2013 Bizzby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 20131209_iOS_ViewControllers_Containment/ViewControllerContainment/ViewControllerContainment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewControllerContainment
+//
+//  Created by Gio on 09/12/2013.
+//  Copyright (c) 2013 Bizzby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VCCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VCCAppDelegate class]));
+    }
+}
