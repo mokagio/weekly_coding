96a55afb19096e343fa1a0da133e41d35c25707f
Gio <giovanni.lodi42@gmail.com>
2013-12-16 15:19:40 +0000

    UIModalPresentationCustom, empty default project

---
+++ 20131216_iOS_UIModalPresentationCustom/UIModalPresentationCustom/UIModalPresentationCustom/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UIModalPresentationCustom
+//
+//  Created by Gio on 16/12/2013.
+//  Copyright (c) 2013 mokagio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
