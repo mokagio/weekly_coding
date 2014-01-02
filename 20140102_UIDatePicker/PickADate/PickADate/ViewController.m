de1de96d2072b3d881c6493ee7bd1ca53776b0a7
Gio <giovanni.lodi42@gmail.com>
2014-01-02 12:20:25 +0000

    Added datepicker with event tracking

--- 20140102_UIDatePicker/PickADate/PickADate/ViewController.m
+++ 20140102_UIDatePicker/PickADate/PickADate/ViewController.m
@@ -17,13 +17,34 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // I was wandering if we can actually change the height of the picker...
+//
+//    CGRect datePickerFrame = CGRectMake(0, 0, self.view.frame.size.width / 2, 50);
+//    UIDatePicker *datePicker = [[UIDatePicker alloc] initWithFrame:datePickerFrame];
+//    datePicker.layer.borderColor = [UIColor redColor].CGColor;
+//    datePicker.layer.borderWidth = 1;
+//    
+    // ...you can't
+
+    UIDatePicker *datePicker = [[UIDatePicker alloc] init];
+    datePicker.center = self.view.center;
+    
+    // We have to configure the UIDatePicker with a selector to handle it's events
+    // Form the docs
+//    When properly configured, a UIDatePicker object sends an action message when a user finishes
+//    rotating one of the wheels to change the date or time; the associated control event is
+//    UIControlEventValueChanged.
+    [datePicker addTarget:self
+                   action:@selector(datePickerValueChanged:)
+         forControlEvents:UIControlEventValueChanged];
+    
+    [self.view addSubview:datePicker];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)datePickerValueChanged:(UIDatePicker *)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSLog(@"%@", sender.date);
 }
 
 @end
